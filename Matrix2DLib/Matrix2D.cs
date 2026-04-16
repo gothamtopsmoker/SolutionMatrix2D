7a64e61f805c434aef61b58180d8f4bc9255dea8
Maksym Heraskin <maksym.heraskin@microsoft.wsei.edu.pl>
2026-04-16 20:05:10 +0200

    all functionality

--- Matrix2DLib/Matrix2D.cs
+++ Matrix2DLib/Matrix2D.cs
@@ -19,24 +19,38 @@
         }
 
         public Matrix2D() : this(1, 0, 0, 1) { }
-        
-        public static Matrix2D Id => new Matrix2D();
-        public static Matrix2D Zero => new Matrix2D(0, 0, 0, 0);
+
+        public static Matrix2D Id { get; } = new Matrix2D();
+public static Matrix2D Zero { get; } = new Matrix2D(0, 0, 0, 0);
 
         public override string ToString() => $"[[{a}, {b}], [{c}, {d}]]";
+
+        public static Matrix2D Parse(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) throw new FormatException();
+
+            var match = System.Text.RegularExpressions.Regex.Match(s.Trim(), @"^\[?\s*\[\s*(-?\d+)\s*,\s*(-?\d+)\s*\]\s*,\s*\[\s*(-?\d+)\s*,\s*(-?\d+)\s*\]\s*\]?$");
+            if (!match.Success) throw new FormatException();
+
+            int a = int.Parse(match.Groups[1].Value);
+            int b = int.Parse(match.Groups[2].Value);
+            int c = int.Parse(match.Groups[3].Value);
+            int d = int.Parse(match.Groups[4].Value);
+
+            return new Matrix2D(a, b, c, d);
+        }
 
 
         #region rownosc
         public bool Equals(Matrix2D? other)
         {
+            if (ReferenceEquals(this, other)) return true;
             if (other is null) return false;
             return a == other.a && b == other.b && c == other.c && d == other.d;
         }
-        override public bool Equals(object? obj)
-        {
-            if (obj is null) return false;
-            return obj is Matrix2D m && Equals(m);
-        }
+
+        public override bool Equals(object? obj) => obj is Matrix2D m && Equals(m);
+
         public override int GetHashCode() => HashCode.Combine(a, b, c, d);
 
         public static bool operator ==(Matrix2D? left, Matrix2D? right)
@@ -44,6 +58,7 @@
             if (left is null) return right is null;
             return left.Equals(right);
         }
+
         public static bool operator !=(Matrix2D? left, Matrix2D? right) => !(left == right);
         #endregion
 
@@ -66,10 +81,19 @@
         public static Matrix2D operator *(Matrix2D m, int k) =>
             new Matrix2D(m.a * k, m.b * k, m.c * k, m.d * k);
 
-        public static Matrix2D operator -(Matrix2D m) =>
-            new Matrix2D(-m.a, -m.b, -m.c, -m.d);
+        public static Matrix2D operator -(Matrix2D m) => -1 * m;
         #endregion
 
-       
+        #region inne operacje
+        public static Matrix2D Transpose(Matrix2D matrix) => new Matrix2D(matrix.a, matrix.c, matrix.b, matrix.d);
+        public Matrix2D Transpose() => Transpose(this);
+
+        public static int Determinant(Matrix2D matrix) => matrix.a * matrix.d - matrix.b * matrix.c;
+        public int Det() => Determinant(this);
+        #endregion
+
+        #region rzutowania
+        public static explicit operator int[,](Matrix2D m) => new int[,] { { m.a, m.b }, { m.c, m.d } };
+        #endregion
     }
 }
